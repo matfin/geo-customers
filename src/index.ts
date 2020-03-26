298cd9adc03a9c9bad1d3861485b39d1fe2c4063
Matt Finucane <matfin@users.noreply.github.com>
2020-03-26 21:14:39 +0000

    feat: display list of sorted and filtered users (#3)

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,30 @@
+import config from './config';
 import { fetchCustomers } from './utils/api';
-import { ICustomer } from './interfaces';
+import { haversineDistanceInKm } from './utils/distances';
+import { ICustomer, ICoordinates } from './interfaces';
 
 const run = async(): Promise<void> => {
-  const result: ICustomer[] = await fetchCustomers('/intercom-take-home-test/customers.txt');
+  const officeCoordinates: ICoordinates = config.coordinates;
+  const distanceKm: number = config.distanceKm;
 
-  console.log(result);
+  // Get the full list of customers and remap to get their distance from the office
+  const customers: ICustomer[] = await fetchCustomers('/intercom-take-home-test/customers.txt');
+  const remapped =
+    customers.map((customer: ICustomer) => {
+      return {
+        customer,
+        distance: haversineDistanceInKm(customer.location, officeCoordinates)
+      };
+    });
+
+  // Then apply filtering 
+  const filtered = remapped.filter((item) => item.distance <= 100);
+
+  // Then apply sorting by user ID
+  const sorted = filtered.sort((a, b) => a.customer.user_id - b.customer.user_id );
+  
+  // Then output the name and user ID
+  console.log(sorted.map(item => `${item.customer.name} [${item.customer.user_id}]`));
 };
 
 run();
