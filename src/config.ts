2eb2fc879853adaef227b727aea7901e98770800
Matt Finucane <matfin@gmail.com>
2020-03-26 20:34:50 +0000

    feat(distance): added function to calculate haversine distance

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,16 @@
+import { ICoordinates } from './interfaces';
+
 interface IAppConfig {
   baseUrl: string,
+  coordinates: ICoordinates,
 }
 
 const config: IAppConfig = {
-  baseUrl: process.env.BASE_URL || 'https://s3.amazonaws.com'
+  baseUrl: process.env.BASE_URL || 'https://s3.amazonaws.com',
+  coordinates: {
+    latitude: parseFloat(process.env.lat) || 53.339428,
+    longitude: parseFloat(process.env.lng) || -6.257664,
+  }
 } 
 
 export default config;
